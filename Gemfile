072258fcef37ca2379d59ad56d37b832ba767b38
Nat Welch <nat@natwelch.com>
2017-02-26 10:14:19 -0800

    update update

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+ruby "2.4.0"
+
+gem "front_matter_parser"
--- update.sh
+++ update.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
+RUBY=~/.rvm/environments/ruby-2.4.0
+
+if [[ ! -f $RUBY ]] ; then
+  echo "File $RUBY is not there, aborting."
+  exit
+fi
+
+source $RUBY
+
 git pull
-rm posts/*.md
+bundle update
+git ci Gemfile* -m 'bundle update'
+rm _posts/*.md
 ./grab_all.rb
-git add posts
+git add _posts
 git ci -m "Backing up posts"
 git push
