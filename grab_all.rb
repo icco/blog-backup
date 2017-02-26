4c211f48576a08a40da64013a660f357ba180edd
Nat Welch <nat@natwelch.com>
2017-02-26 10:13:54 -0800

    Backing up posts

--- grab_all.rb
+++ grab_all.rb
@@ -4,8 +4,9 @@
 
 require "open-uri"
 require "json"
+require "front_matter_parser"
+require "time"
 
-#CRLF_REGEX = Regexp.new("(?<!\r)\n|\r(?!\n)")
 CRLF_REGEX = /\r\n?/
 
 open("https://writing.natwelch.com/posts.md.json") do |res|
@@ -17,10 +18,15 @@ open("https://writing.natwelch.com/posts.md.json") do |res|
   threads = 10.times.map do
     Thread.new do
       while !queue.empty? && u = queue.pop
-        filename = "posts/#{File.basename(u)}"
         open("https://writing.natwelch.com#{u.sub(".", "/")}") do |r|
+          body = r.read.gsub(CRLF_REGEX, "\n")
+          fmp = FrontMatterParser.parse(body)
+          fm = fmp.front_matter
+          p fm
+          dt = Time.parse(fm["datetime"])
+          filename = File.join "_posts/", "#{dt.strftime "%Y-%m-%d"}-#{File.basename(u)}"
           File.open(filename, 'w') do |f|
-            f.write(r.read.gsub(CRLF_REGEX, "\n"))
+            f.write(body)
           end
         end
       end
