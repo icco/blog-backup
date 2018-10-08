fd4993b004d01294de417cd7889d08efe20beed8
Nat Welch <nat@natwelch.com>
2018-10-08 15:40:19 +0000

    Move graball to graphql

--- grab_all.rb
+++ grab_all.rb
@@ -8,39 +8,58 @@ Bundler.require(:default)
 require "open-uri"
 require "json"
 require "time"
+require "graphql/client"
+require "graphql/client/http"
 
 CRLF_REGEX = /\r\n?/
 
-open("https://natwelch-writing.appspot.com/posts.md.json") do |res|
-
-  queue = Queue.new
-  urls = JSON.parse(res.read)
-  urls.map { |url| queue << url }
-
-  threads = 10.times.map do
-    Thread.new do
-      while !queue.empty? && u = queue.pop
-        open("https://natwelch-writing.appspot.com#{u.sub(".", "/")}") do |r|
-          begin
-            body = r.read.gsub(CRLF_REGEX, "\n")
-            prsr = FrontMatterParser::Parser.new(:md)
-            fmp = prsr.call(body)
-            fm = fmp.front_matter
-            p fm
-            dt = Time.parse(fm["datetime"])
-            filename = File.join "_posts/", "#{dt.strftime "%Y-%m-%d"}-#{File.basename(u)}"
-            File.open(filename, "w") do |f|
-              f.write(body)
-            end
-          rescue Exception => e
-            p u
-            puts e.message
-            puts e.backtrace.inspect
-          end
-        end
-      end
-    end
+http = GraphQL::Client::HTTP.new("https://graphql.natwelch.com/graphql") do
+  def headers(context)
+    { "User-Agent": "blog-backup" }
   end
+end
+schema = GraphQL::Client.load_schema(http)
+client = GraphQL::Client.new(schema: schema, execute: http)
+Query = client.parse <<-'GRAPHQL'
+  query {
+    allPosts {
+      id
+      title
+      content
+      datetime
+      draft
+    }
+  }
+GRAPHQL
 
-  threads.each(&:join)
+res = client.query(Query)
+res.data.all_posts.each do |post|
+  dt = Time.parse(post.datetime)
+  filename = File.join "_posts/", "#{dt.strftime "%Y-%m-%d"}-#{post.id}.md"
+  p filename
+
+  body = <<-HEAD
+---
+
+id: #{post.id}
+datetime: "#{dt}"
+title: "#{post.title}"
+draft: #{post.draft}
+permalink: "/post/#{post.id}"
+
+---
+
+#{post.content}
+HEAD
+
+  # Validate yaml
+  prsr = FrontMatterParser::Parser.new(:md)
+  fmp = prsr.call(body)
+  fm = fmp.front_matter
+  p fm
+
+  # Write that shit
+  File.open(filename, "w") do |f|
+    f.write(body)
+  end
 end
